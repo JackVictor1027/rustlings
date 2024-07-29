7679921cedc9769bf60ba55dde324d49902d70cb
jackvictor <jackvictor027@163.com>
2024-07-29 10:24:53 +0800

    rust test 7

--- exercises/07_structs/structs1.rs
+++ exercises/07_structs/structs1.rs
@@ -1,9 +1,13 @@
 struct ColorRegularStruct {
     // TODO: Add the fields that the test `regular_structs` expects.
     // What types should the fields have? What are the minimum and maximum values for RGB colors?
+    red: i32,
+    green: i32,
+    blu: i32,
 }
 
-struct ColorTupleStruct(/* TODO: Add the fields that the test `tuple_structs` expects */);
+// struct ColorTupleStruct(/* TODO: Add the fields that the test `tuple_structs` expects */);
+struct ColorTupleStruct(i32, i32, i32);
 
 #[derive(Debug)]
 struct UnitStruct;
@@ -20,17 +24,21 @@ mod tests {
     fn regular_structs() {
         // TODO: Instantiate a regular struct.
         // let green =
-
+        let green = ColorRegularStruct {
+            red: 0,
+            green: 255,
+            blu: 0,
+        };
         assert_eq!(green.red, 0);
         assert_eq!(green.green, 255);
-        assert_eq!(green.blue, 0);
+        assert_eq!(green.blu, 0);
     }
 
     #[test]
     fn tuple_structs() {
         // TODO: Instantiate a tuple struct.
         // let green =
-
+        let green = ColorTupleStruct(0, 255, 0);
         assert_eq!(green.0, 0);
         assert_eq!(green.1, 255);
         assert_eq!(green.2, 0);
@@ -39,7 +47,7 @@ mod tests {
     #[test]
     fn unit_structs() {
         // TODO: Instantiate a unit struct.
-        // let unit_struct =
+        let unit_struct = UnitStruct;
         let message = format!("{unit_struct:?}s are fun!");
 
         assert_eq!(message, "UnitStructs are fun!");
